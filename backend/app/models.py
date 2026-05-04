4af2cd51640385430ef1698b4c0328b0ebda16ca
diegoyupanquic-commits <diego.yupanquic@unmsm.edu.pe>
2026-05-04 11:08:40 -0500

    estructura de models.py para la base de datos

---
+++ backend/app/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from .database import Base
+
+class UsuarioDB(Base):
+    __tablename__ = "usuarios"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String) 
+    
+    incidentes = relationship("IncidenteDB", back_populates="usuario")
+
+class IncidenteDB(Base):
+    __tablename__ = "incidentes"
+    id = Column(Integer, primary_key=True, index=True)
+    tipo = Column(String, index=True) 
+    latitud = Column(Float, nullable=False)
+    longitud = Column(Float, nullable=False)
+    nivel_urgencia = Column(Integer, default=1) 
+    descripcion = Column(String, nullable=True)
+    fecha_reporte = Column(DateTime, default=datetime.utcnow)
+    
+    
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"))
+    
+    usuario = relationship("UsuarioDB", back_populates="incidentes")
